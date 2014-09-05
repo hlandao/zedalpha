a3fb9d33073e2969bc8200776c84ba4e248433ea
Hadar Landao <hlandao@gmail.com>
2014-09-05 18:11:42 +0300

    businesses list links to events management

--- app/scripts/directives/timepicker-directive.js
+++ app/scripts/directives/timepicker-directive.js
@@ -52,7 +52,6 @@ zedAlphaDirectives
 
                     $scope.$watch('settings', function(newVal,oldVal){
                         settings = angular.extend(defaultSettings, $scope.settings);
-                        console.log('newVal',newVal);
                         var max = (newVal && newVal.min && newVal.range) ? newVal.min.clone().add(newVal.range, 'minutes') : null ;
                         if(newVal && newVal.min && newVal.min.isAfter && newVal.min.isAfter(ngModel.$modelValue)){
                             ngModel.$modelValue = newVal.min.clone();
