d245852a5cd955ce2d092df2e8f5b5cd1b59aa2c
Hadar Landao <hlandao@gmail.com>
2014-09-16 23:38:33 +0300

    deploy --to BUILD_NAME

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -1,55 +1,65 @@
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
 var fs = require('fs');
 var awspublish = require('gulp-awspublish');
-var awsDetails = JSON.parse(fs.readFileSync('./.ignored/aws.json'));
 
-var awsPublisherStaging = awspublish.create({
-    key: awsDetails.key,
-    secret: awsDetails.secret,
-    bucket: awsDetails.bucketStaging
-});
-
-var awsPublisherStaging1 = awspublish.create({
-    key: awsDetails.key,
-    secret: awsDetails.secret,
-    bucket: awsDetails.bucketStaging1
-});
-
-var awsPublisherProduction = awspublish.create({
-    key: awsDetails.key,
-    secret: awsDetails.secret,
-    bucket: awsDetails.bucketProduction
-});
 
 var awsHeaders = { 'Cache-Control': 'max-age=315360000, no-transform, public' };
 
-console.log('awsDetails',awsDetails);
-
 // aws staging deployment
-gulp.task('deploy:staging', function(){
-    return gulp.src('./dist/**')
+var to, envs, promptMsg,awsDetails,toEnv;
+gulp.task('deploy', function(){
+    awsDetails = JSON.parse(fs.readFileSync('./.ignored/aws.json'));
+    to = $.util.env.to;
+    envs = JSON.parse(fs.readFileSync('./.ignored/envs.json'));
 
-        .pipe(awsPublisherStaging.publish(awsHeaders))
-        .pipe(awsPublisherStaging.sync())  // sync local directory with bucket
+    if(!to || !envs.envs[to]){
+        var options = "";
+        for(var i in envs.envs){
+            if(options){
+                options += ",";
+            }
+            options += i;
+        }
+        throw new Error("Please specify a valid environment. 'gulp deploy --to ENVIRONMENT_NAME'. ("+options+")");
+    }
+
+    toEnv = envs.envs[to];
+    promptMsg = "Are you sure you want to deploy to " + to.toUpperCase() + "(bucket = "+toEnv.bucket+", firebase = "+toEnv.firebase_url+") ?";
+    runSequence('deploy-prompt', 'build', 'deploy-config', 'deploy-do');
+});
+
+gulp.task('deploy-prompt', function(){
+    return gulp.src('').pipe($.prompt.confirm({message : promptMsg}));
+});
+
+gulp.task('deploy-config', function(){
+    return gulp
+        .src('./dist/scripts/config.json')
+        .pipe($.jsonEditor(function(json) {
+            json.firebase_url = toEnv.firebase_url;
+            return json; // must return JSON object.
+        }))
+        .pipe(gulp.dest("./dist/scripts/"));
+
+});
+
+
+gulp.task('deploy-do', function(){
+
+    var publisher = awspublish.create({
+        key : awsDetails.key,
+        secret  : awsDetails.secret,
+        bucket : toEnv.bucket
+    });
+
+    return gulp
+        .src('./dist/**')
+        .pipe(publisher.publish(awsHeaders))
+        .pipe(publisher.sync())  // sync local directory with bucket
         //.pipe(awsPublisher.cache()) // create a cache file to speed up next uploads
         .pipe(awspublish.reporter()); // print upload updates to console
 });
 
-// aws staging1 deployment
-gulp.task('deploy:staging1',['build'], function(){
-    return gulp.src('./dist/**')
-        .pipe(awsPublisherStaging1.publish(awsHeaders))
-        .pipe(awsPublisherStaging1.sync())  // sync local directory with bucket
-        //.pipe(awsPublisher.cache()) // create a cache file to speed up next uploads
-        .pipe(awspublish.reporter()); // print upload updates to console
-});
-
-// aws production deployment
-gulp.task('deploy:production',['build'], function(){
-    return gulp.src('./dist/**')
-        .pipe(awsPublisherProduction.publish(awsHeaders))
-        .pipe(awsPublisherProduction.sync())  // sync local directory with bucket
-        //.pipe(awsPublisher.cache()) // create a cache file to speed up next uploads
-        .pipe(awspublish.reporter()); // print upload updates to console
-});
 
